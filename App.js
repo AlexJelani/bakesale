22bcea4e2004e1d89d5ae03233b82141b1fdab60
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-05-29 19:36:54 +0900

    Implement the Back link

--- App.js
+++ App.js
@@ -1,43 +1,51 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
 import ajax from './src/ajax';
-import DealList from './src/components/DealList.jsx';
+import DealList from './src/components/DealList';
 import DealDetail from './src/components/DealDetail';
 
-class App extends Component {
+class App extends React.Component {
   state = {
     deals: [],
     currentDealId: null,
   };
-
   async componentDidMount() {
-    const fetchedDeals = await ajax.fetchInitialDeals();
-    this.setState({ deals: fetchedDeals });
+    const deals = await ajax.fetchInitialDeals();
+    this.setState({ deals });
   }
-
   setCurrentDeal = (dealId) => {
-    this.setState({ currentDealId: dealId });
+    this.setState({
+      currentDealId: dealId,
+    });
   };
-
+  unsetCurrentDeal = () => {
+    this.setState({
+      currentDealId: null,
+    });
+  };
   currentDeal = () => {
     return this.state.deals.find((deal) => deal.key === this.state.currentDealId);
   };
-
   render() {
-    const { deals, currentDealId } = this.state;
-
-    if (currentDealId) {
-      return <DealDetail initialDealData={this.currentDeal()} />;
-    } else if (deals.length > 0) {
-      return <DealList deals={deals} onItemPress={this.setCurrentDeal} />;
-    } else {
+    if (this.state.currentDealId) {
       return (
-        <View style={styles.container}>
-          <Text style={styles.paragraph}>Bakesale</Text>
-        </View>
+        <DealDetail
+          initialDealData={this.currentDeal()}
+          onBack={this.unsetCurrentDeal}
+        />
       );
     }
+    if (this.state.deals.length > 0) {
+      return (
+        <DealList deals={this.state.deals} onItemPress={this.setCurrentDeal} />
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Bakesale</Text>
+      </View>
+    );
   }
 }
 
@@ -46,11 +54,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
   },
-  paragraph: {
+  header: {
     fontSize: 40,
   },
 });
