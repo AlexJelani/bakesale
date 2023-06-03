8fb61292d5e5b798e87b77cf06978afca94b1913
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-06-03 19:07:08 +0900

    Implemented the search feature

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,42 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import debounce from 'lodash.debounce';
 import { TextInput, StyleSheet } from 'react-native';
 
 class SearchBar extends React.Component {
+  static propTypes = {
+    searchDeals: PropTypes.func.isRequired
+  };
+
+  state = {
+    searchTerm: ''
+  };
+
+  debouncedSearchDeals = debounce(this.props.searchDeals, 300);
+
+  handleChange = (searchTerm) => {
+    this.setState({ searchTerm }, () => {
+      // debounce the search
+      this.debouncedSearchDeals(this.state.searchTerm);
+    });
+  };
+
   render() {
-    return <TextInput placeholder="Search All Deals" style={styles.input} />;
+    return (
+      <TextInput
+        placeholder="Search All Deals"
+        style={styles.input}
+        onChangeText={this.handleChange}
+      />
+    );
   }
 }
 
 const styles = StyleSheet.create({
   input: {
     height: 40,
-    marginHorizontal: 12,
-  },
+    marginHorizontal: 12
+  }
 });
 
 export default SearchBar;
